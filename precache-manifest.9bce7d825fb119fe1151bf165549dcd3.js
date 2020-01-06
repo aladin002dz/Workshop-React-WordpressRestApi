edca8a36169e5deee2ddee45c5c8e003470e2683
Mahfoudh AROUS <mahfoudh.arous@gmail.com>
2020-01-06 21:33:36 +0100

    update deploy

---
+++ precache-manifest.9bce7d825fb119fe1151bf165549dcd3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbacc75ad9cd040111fed8fecc36a0ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ced3c2c0fe1063311746",
+    "url": "/static/js/2.5e6f2eb9.chunk.js"
+  },
+  {
+    "revision": "f4e0edc10110ebcce0ec",
+    "url": "/static/js/main.3eee0a2a.chunk.js"
+  },
+  {
+    "revision": "27feb39b0cfd9c6d1e7e",
+    "url": "/static/js/runtime-main.66224fb5.js"
+  }
+]);
